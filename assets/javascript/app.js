f5b696860b91fa9632785977cde08856c7910c37
SarahReyes <siwreyes@gmail.com>
2016-08-29 16:39:18 -0500

    pseudocode, getting started

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+
+// KEY: "dc6zaTOxFJmzC”
+
+// create a data-still attribute with the URL for the
+// still image and a data-animate attribute for the URL of the
+// animated image. Then you just change the source to one of
+// those two values depending on what you want it to do. You
+// might check out  the in-class activity from 6.3 / 04 (HINT HINT)
+
+// create multiple buttons with TV show names
+// use an array to loop over all of the TV show buttons
+// label the button with the TV show name
+// connect the button to the giphy API
+// create a bootstrap form for someone to submit a TV show they want to see
+// limit the amount of gifs to 5
+// when the gif pops up, it is paused
+// user can click the gif and it plays
+// user can click on the gif again and it pauses
+// when the user clicks a new button, the previous gif go away
+// rating will display with the gif, up to pg-13
+
+var tvShows = ["30 Rock", "How I Met Your Mother", "Downton Abbey", "Mr. Robot", "Mad Men"];
+
+    var queryURL = "https://api.giphy.com/
+
+
+      $(document).ready(function() {
+
+
+
+      });
